4914a22166600a6d4984be787998bf18c50f1faa
David Mottola <davidaubin02@gmail.com>
2026-05-25 20:17:29 +0200

    connexion fonctionnel

---
+++ FrontEnd/login.js
@@ -0,0 +1,26 @@
+const loginForm = document.querySelector(".loginForm")
+
+loginForm.addEventListener("submit", async function (event) {
+
+    event.preventDefault();
+    const email = document.getElementById("email").value;
+    const motDepasse = document.getElementById("motDePasse").value;
+    const login ={
+    email: email,
+    password:motDepasse
+}; console.log(login)
+const reponseAPI = await fetch ("http://localhost:5678/api/users/login",{
+    method:"POST",
+    headers:{"Content-Type": "application/json"},
+    body: JSON.stringify(login)
+});
+const data = await reponseAPI.json();
+if (reponseAPI.ok){
+console.log(data);
+window.location.href = "index.html";
+}else {
+    document.querySelector (".erreurLogin") .innerText= "Erreur d'identifiant ou de mot de passe";
+}
+});
+
+
